624812a8aded93cff3309f9686709a9fe712e5ce
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-01-21 18:15:44 -0300

    build: dependência do postgre para deploy no heroku

---
+++ src/main/java/com/anderson/banco/config/BdConfig.java
@@ -0,0 +1,25 @@
+package com.anderson.banco.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class BdConfig {
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
