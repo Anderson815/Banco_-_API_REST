d31ce69a901bb8b6beaf4a49c18eb43c17330ddd
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-01-30 09:23:36 -0300

    refactor: alterado a menssagem da validação por tamanho do RG

--- src/main/java/com/anderson/banco/model/request/ContaModelRequest.java
+++ src/main/java/com/anderson/banco/model/request/ContaModelRequest.java
@@ -9,7 +9,7 @@ public class ContaModelRequest {
     @NotBlank(message = "O nome não foi informado") //validação de nulo ou vazio (só para Strings)
     private String nome;
     @NotNull(message = "O RG não foi informado") //validação de nulo (para objetos)
-    @Size(min = 9, max = 9, message = "O RG não é valido, verifique se o mesmo está correto") //validação de tamanho (só para Strings)
+    @Size(min = 9, max = 9, message = "O RG deve conter exatamente 9 caracteres") //validação de tamanho (só para Strings)
     private String rg;
 
     public String getNome() {
