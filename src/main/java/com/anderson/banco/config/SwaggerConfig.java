4129da1e2fe25eed6cc1b11c8952aed448f3749d
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-03-26 11:07:27 -0300

    docs(SwaggerConfig): adicionado o contato e alterado a licença

--- src/main/java/com/anderson/banco/config/SwaggerConfig.java
+++ src/main/java/com/anderson/banco/config/SwaggerConfig.java
@@ -1,18 +1,14 @@
 package com.anderson.banco.config;
 
-import io.swagger.models.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.VendorExtension;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -32,13 +28,15 @@ public class SwaggerConfig {
 
     public ApiInfo metaInfo(){
 
+        Contact contato = new springfox.documentation.service.Contact("Anderson", "https://www.linkedin.com/in/anderson-correia", "anderson_souza33@outlook.com.br");
+
         return new ApiInfoBuilder()
                 .title("Banco API REST")
                 .description("API REST de uma instituição financeira")
                 .version("1.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .license("MIT License")
+                .licenseUrl("https://github.com/Anderson815/Banco_-_API_REST/blob/master/LICENSE")
+                .contact(contato)
                 .build();
-
     }
 }
