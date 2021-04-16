44c20cba03d6ebc20f6c3aef90b1ee884538196d
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-04-16 14:36:19 -0300

    docs(ContaController): adicionado informações dos parâmetros no Swagger

--- src/main/java/com/anderson/banco/controller/ContaController.java
+++ src/main/java/com/anderson/banco/controller/ContaController.java
@@ -10,10 +10,7 @@ import com.anderson.banco.model.request.ContaModelRequest;
 import com.anderson.banco.model.response.ContaModelResponse;
 import com.anderson.banco.service.ContaService;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,7 +34,7 @@ public class ContaController {
 			@ApiResponse(code = 200, message = "Retorna o produto informado"),
 			@ApiResponse(code = 404, message = "Não existe conta com o uuid informado")
 	})
-	public ResponseEntity<ContaModelResponse> getConta(@PathVariable String uuid){
+	public ResponseEntity<ContaModelResponse> getConta(@ApiParam(value = "UUID da conta que será consultado") @PathVariable String uuid){
 		return new ResponseEntity<>(contaService.getConta(uuid), HttpStatus.OK);
 	}
 
@@ -54,7 +51,7 @@ public class ContaController {
 			@ApiResponse(code = 201, message = "Conta cadastrada"),
 			@ApiResponse(code = 400, message = "Não foi passado os atributos do objeto de forma adequada")
 	})
-	public ResponseEntity<ContaModelResponse> criarConta(@Valid @RequestBody ContaModelRequest contaModelRequest, BindingResult erroRequest){
+	public ResponseEntity<ContaModelResponse> criarConta(@ApiParam(value = "Objeto conta que será cadastrada no banco") @Valid @RequestBody ContaModelRequest contaModelRequest, BindingResult erroRequest){
 		//BindingResult é para a validação
 		if(erroRequest.hasErrors()) throw new RequestConstraintException(erroRequest.getAllErrors().get(0).getDefaultMessage());
 		return new ResponseEntity<>(contaService.criarConta(contaModelRequest), HttpStatus.CREATED);
@@ -64,10 +61,10 @@ public class ContaController {
 	@ApiOperation(value="Deposita dinheiro para a conta pelo ID")
 	@ApiResponses(value = {
 			@ApiResponse(code = 200, message = "O dinheiro foi depositado"),
-			@ApiResponse(code = 400, message = "•Valor abaixo de um centavo •Valor acima do limite(5000,00"),
+			@ApiResponse(code = 400, message = "•Valor abaixo de um centavo •Valor acima do limite(5000,00)"),
 			@ApiResponse(code = 404, message = "Não existe conta com o uuid informado")
 	})
-	public ResponseEntity<ContaModelResponse> depositarDinheiro(@PathVariable String uuid, @RequestParam(value = "valor") BigDecimal valor){
+	public ResponseEntity<ContaModelResponse> depositarDinheiro(@ApiParam(value = "UUID da conta que vai depositar") @PathVariable String uuid, @ApiParam(value = "Valor para fazer a operação de deposito") @RequestParam(value = "valor") BigDecimal valor){
 		return new ResponseEntity<>(contaService.depositarDinheiro(uuid, valor), HttpStatus.OK);
 	}
 
@@ -78,7 +75,7 @@ public class ContaController {
 			@ApiResponse(code = 400, message = "•Valor abaixo de um centavo •Valor acima da quantidade depositada na conta"),
 			@ApiResponse(code = 404, message = "Não existe conta com o uuid informado")
 	})
-	public ResponseEntity<ContaModelResponse> sacarDinheiro(@PathVariable String uuid, @RequestParam(value = "valor") BigDecimal valor){
+	public ResponseEntity<ContaModelResponse> sacarDinheiro(@ApiParam(value = "UUID da conta que vai sacar") @PathVariable String uuid, @ApiParam(value = "Valor para fazer a operação de saque") @RequestParam(value = "valor") BigDecimal valor){
 		return new ResponseEntity<>(contaService.sacarDinheiro(uuid, valor), HttpStatus.OK);
 	}
 
@@ -89,7 +86,7 @@ public class ContaController {
 			@ApiResponse(code = 400, message = "•Valor abaixo de um centavo •Valor de transferência acima da quantidade depositada na conta"),
 			@ApiResponse(code = 404, message = "•Não existe conta com o uuid informado para retirar o dinheiro •Não existe conta com o uuid informado para receber o dinheiro")
 	})
-	public ResponseEntity<ContaModelResponse> transferir(@PathVariable(value = "uuidRetira") String uuidRetira, @RequestParam(value = "uuidRecebe") String uuidRecebe, @RequestParam(value = "valor") BigDecimal valor){
+	public ResponseEntity<ContaModelResponse> transferir(@ApiParam(value = "UUID da conta que vai fazer a transferência") @PathVariable(value = "uuidRetira") String uuidRetira, @ApiParam(value = "UUID da conta que vai receber a transferência") @RequestParam(value = "uuidRecebe") String uuidRecebe, @ApiParam(value = "Valor para fazer a operação de transferência") @RequestParam(value = "valor") BigDecimal valor){
 		return new ResponseEntity<>(contaService.tranferir(uuidRetira, uuidRecebe, valor), HttpStatus.OK);
 	}
 
@@ -100,7 +97,7 @@ public class ContaController {
 			@ApiResponse(code = 400, message = "Existe dinheiro para ser sacado na conta informada"),
 			@ApiResponse(code = 404, message = "Não existe conta com o uuid informado")
 	})
-	public ResponseEntity<Object> deletarConta(@PathVariable String uuid){
+	public ResponseEntity<Object> deletarConta(@ApiParam(value = "UUID da conta que será deletada") @PathVariable String uuid){
 		contaService.deletarConta(uuid);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);		
 	}
@@ -116,7 +113,7 @@ public class ContaController {
 					"•Não foi passado os atributos do objeto de forma adequada"),
 			@ApiResponse(code = 404, message = "Não existe conta com o uuid informado")
 	})
-	public ResponseEntity<CompraModelResponse> criarCompra(@PathVariable String uuid, @Valid @RequestBody CompraModelRequest compraModelRequest, BindingResult erroRequest){
+	public ResponseEntity<CompraModelResponse> criarCompra(@ApiParam(value = "UUID da conta que vai comprar") @PathVariable String uuid, @ApiParam(value = "Objeto compra que vai ser cadastrado na conta informada") @Valid @RequestBody CompraModelRequest compraModelRequest, BindingResult erroRequest){
 		if(erroRequest.hasErrors()) throw new RequestConstraintException(erroRequest.getAllErrors().get(0).getDefaultMessage());
 		return new ResponseEntity<>(contaService.criarCompra(uuid, compraModelRequest), HttpStatus.CREATED);
 	}
@@ -127,7 +124,7 @@ public class ContaController {
 			@ApiResponse(code = 200, message = "Lista das compras da conta informada"),
 			@ApiResponse(code = 404, message = "Não existe conta com o uuid informado")
 	})
-	public ResponseEntity<List<CompraModelResponse>> getCompras(@PathVariable String uuid){
+	public ResponseEntity<List<CompraModelResponse>> getCompras(@ApiParam(value = "UUID da conta que possui as compras") @PathVariable String uuid){
 		return new ResponseEntity<>(contaService.getCompras(uuid), HttpStatus.OK);
 	}
 
@@ -138,7 +135,7 @@ public class ContaController {
 			@ApiResponse(code = 404, message = "•Não existe conta com o uuid informado " +
 					"•Não existe compra com o id_compra informado")
 	})
-	public ResponseEntity<CompraModelResponse> getCompra(@PathVariable(value = "uuid") String uuid, @PathVariable(value = "id_compra") int id_compra){
+	public ResponseEntity<CompraModelResponse> getCompra(@ApiParam(value = "UUID da conta que possui a compra") @PathVariable(value = "uuid") String uuid, @ApiParam(value = "ID da compra que será exibida") @PathVariable(value = "id_compra") int id_compra){
 		return new ResponseEntity<>(contaService.getCompra(uuid, id_compra), HttpStatus.OK);
 	}
 }
